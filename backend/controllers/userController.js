1394d162f0f855f29723b3efb368bdd026de2b5c
OmarElZaher <omarelzaher93@gmail.com>
2023-11-23 23:57:51 +0200

    added create owner with pets

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,18 @@
+// Imports
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
+
 const User = require("../models/SystemModels/userModel");
+const Owner = require("../models/CustomerModels/ownerModel");
+const Pet = require("../models/CustomerModels/petModel");
+
+// ----------------------------------------------------------------
 
 // @desc Create A New User
 // @route POST /user/createUser
 // @access Public
 const createUser = asyncHandler(async (req, res, next) => {
-	// TODO: Handle password requirements / email validation (regex) and check tokens/cookies
-
 	const { username, email, firstName, lastName, password, confirmPassword } =
 		req.body;
 
@@ -23,6 +27,23 @@ const createUser = asyncHandler(async (req, res, next) => {
 	) {
 		res.status(400);
 		throw new Error("Please Enter All Fields");
+	}
+
+	// Validate email format using a regular expression
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!emailRegex.test(email)) {
+		res.status(400);
+		throw new Error("Invalid email format");
+	}
+
+	// Password requirements (customize based on your requirements)
+	const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.{8,})/;
+
+	if (!passwordRegex.test(password)) {
+		res.status(400);
+		throw new Error(
+			"Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, and one digit"
+		);
 	}
 
 	// Validation Checks
@@ -58,7 +79,7 @@ const createUser = asyncHandler(async (req, res, next) => {
 			email: user.email,
 			firstName: user.firstName,
 			lastName: user.lastName,
-			token: generateToken(user._id),
+			// token: generateToken(user._id),
 		});
 	} else {
 		res.status(400);
@@ -69,7 +90,44 @@ const createUser = asyncHandler(async (req, res, next) => {
 // @desc Update User Credentials
 // @route PATCH /user/updateProfile
 // @access Private
-const updateProfile = asyncHandler(async (req, res) => {});
+const updateUserProfile = asyncHandler(async (req, res) => {
+	try {
+		const { username, email } = req.body;
+
+		// Check if the updated username is already taken by another user
+		if (username) {
+			const usernameExists = await User.findOne({
+				username,
+				_id: { $ne: req.user._id }, // Exclude the current user from the search
+			});
+			if (usernameExists) {
+				res.status(400).json({ message: "Username is already taken" });
+				return;
+			}
+		}
+
+		// Check if the updated email is already taken by another user
+		if (email) {
+			const emailExists = await User.findOne({
+				email,
+				_id: { $ne: req.user._id }, // Exclude the current user from the search
+			});
+			if (emailExists) {
+				res.status(400).json({ message: "Email is already in use" });
+				return;
+			}
+		}
+
+		const updatedUser = await User.findByIdAndUpdate(req.user._id, req.body);
+
+		if (updatedUser) {
+			res.status(200).json({ message: "User Updated Successfully" });
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
 
 // @desc Delete User
 // @route DELETE /user/deleteUser
@@ -86,7 +144,32 @@ const deleteUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-// @desc Get My Info
+// @desc Login User
+// @route POST /user/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+		res.status(400).json({ message: "Please Enter All Fields" });
+	}
+
+	const user = await User.findOne({ username: username });
+
+	if (!user) {
+		res.status(400).json({ message: "Invalid Username" });
+	} else {
+		if (await bcrypt.compare(password, user.password)) {
+			res
+				.status(200)
+				.json({ message: "Login Success", token: generateToken(user._id) });
+		} else {
+			res.status(400).json({ message: "Invalid Credentials" });
+		}
+	}
+});
+
+// @desc Get User Info
 // @route GET /user/getUserInfo
 // @access Private
 const getUserInfo = asyncHandler(async (req, res) => {
@@ -100,6 +183,186 @@ const getUserInfo = asyncHandler(async (req, res) => {
 	});
 });
 
+// @desc Create New Owner
+// @route POST /user/createOwner
+// @access Private
+const createOwner = asyncHandler(async (req, res) => {
+	const {
+		firstName,
+		lastName,
+		mobileNumber,
+		email,
+		gender,
+		preferredContactMethod,
+		receiveNotifications,
+	} = req.body;
+
+	if (
+		!firstName ||
+		!lastName ||
+		!mobileNumber ||
+		!email ||
+		!gender ||
+		!preferredContactMethod ||
+		!receiveNotifications
+	) {
+		res.status(400);
+		throw new Error("Please Enter All Fields");
+	}
+
+	try {
+		// Create the owner
+		const owner = await Owner.create(req.body);
+
+		if (owner) {
+			// Update the pets associated with the owner
+			const petIds = req.body.pets; // Assuming you have a field 'pets' in the request body
+
+			if (petIds && Array.isArray(petIds)) {
+				for (const petId of petIds) {
+					const pet = await Pet.findById(petId);
+
+					if (pet) {
+						pet.owners.push(owner._id);
+						await pet.save();
+					}
+				}
+			}
+
+			res.status(200).json({ message: "Owner Created Successfully" });
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
+
+// @desc Get Owner Information
+// @route GET /user/getOwnerInfo/:id
+// @access Private
+const getOwnerInfo = asyncHandler(async (req, res) => {
+	try {
+		const owner = await Owner.findById(req.params.id);
+
+		if (owner) {
+			res.status(400).json(owner);
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error("Owner Not Found");
+	}
+});
+
+// @desc Get Owner
+// @route GET /user/getOwner/
+// @access Private
+const getOwner = asyncHandler(async (req, res) => {
+	try {
+		const owner = await Owner.find(req.body);
+
+		if (owner) {
+			res.status(200).json(owner);
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
+
+// @desc Get Owner Pets
+// @route GET /user/getOwnerPets/:id
+// @access Private
+const getOwnerPets = asyncHandler(async (req, res) => {
+	try {
+		const owner = await Owner.findById(req.params.id).populate("pets");
+
+		if (owner.pets.length > 0) {
+			res.status(200).json(owner.pets);
+		} else {
+			res.status(400).json({ message: "No Pets Found!" });
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
+
+// @desc Update Owner Information
+// @route PATCH /users/updateOwner/:id
+// @access Private
+const updateOwnerProfile = asyncHandler(async (req, res) => {
+	try {
+		const updatedOwner = await Owner.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true }
+		);
+
+		if (updatedOwner) {
+			res.status(200).json({ message: "Owner Updated Successfully" });
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
+
+// @desc Delete Owner
+// @route DELETE /user/deleteOwner/:id
+// @access Private
+const deleteOwnerProfile = asyncHandler(async (req, res) => {
+	try {
+		if (!(await Owner.findByIdAndDelete(req.params.id))) {
+			res.status(400).json({ message: "Owner Not Found!" });
+		} else {
+			res.status(200).json({ message: "Owner Deleted Successfully" });
+		}
+	} catch (error) {
+		res.status(400);
+		throw new Error(error);
+	}
+});
+
+// @desc Create Pet
+// @route POST /user/createPet
+// @access Private
+const createPet = asyncHandler(async (req, res) => {
+	try {
+		const ownerIds = req.body.owners;
+
+		// Check if ownerIds is an array
+		if (!Array.isArray(ownerIds)) {
+			res.status(400).json({ message: "Owners should be an array" });
+			return;
+		}
+
+		// Check if any owner ID is invalid
+		for (const ownerId of ownerIds) {
+			const owner = await Owner.findById(ownerId);
+			if (!owner) {
+				res.status(400).json({ message: `Owner with ID ${ownerId} not found` });
+				return;
+			}
+		}
+
+		// Create the pet
+		const pet = await Pet.create(req.body);
+
+		// Associate the pet with each owner
+		for (const ownerId of ownerIds) {
+			const owner = await Owner.findById(ownerId);
+			owner.pets.push(pet._id);
+			await owner.save();
+		}
+
+		res.status(200).json({ message: "Pet Created Successfully" });
+	} catch (error) {
+		res
+			.status(400)
+			.json({ message: "Error creating pet", error: error.message });
+	}
+});
+
 // Generate Token
 const generateToken = (id) => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -109,6 +372,17 @@ const generateToken = (id) => {
 
 module.exports = {
 	createUser,
+	loginUser,
 	getUserInfo,
 	deleteUserProfile,
+	updateUserProfile,
+
+	createOwner,
+	getOwnerInfo,
+	getOwner,
+	getOwnerPets,
+	updateOwnerProfile,
+	deleteOwnerProfile,
+
+	createPet,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,51 @@
+// Imports
 const express = require("express");
 const router = express.Router();
 
 const {
 	createUser,
+	loginUser,
 	deleteUserProfile,
 	getUserInfo,
+	updateUserProfile,
+
+	createOwner,
+	getOwnerInfo,
+	getOwner,
+	getOwnerPets,
+	deleteOwnerProfile,
+	updateOwnerProfile,
+
+	createPet,
 } = require("../controllers/userController");
 
 const { authenticate } = require("../middleware/authMiddleware");
 
-router.get("/getUserInfo", authenticate, getUserInfo);
+// ----------------------------------------------------------------
 
-router.post("/createUser", createUser);
+// GET methods for /user
+router.get("/getUserInfo", authenticate, getUserInfo); // COMPLETE
+router.get("/getOwnerInfo/:id", authenticate, getOwnerInfo); // COMPLETE
+router.get("/getOwner", authenticate, getOwner); // COMPLETE
+router.get("/getOwnerPets/:id", authenticate, getOwnerPets); // COMPLETE 
 
-router.delete("/deleteUser", authenticate, deleteUserProfile);
+// POST methods for /user
+router.post("/createUser", createUser); // COMPLETE
+router.post("/login", loginUser); // COMPLETE
 
+router.post("/createOwner", authenticate, createOwner); // COMPLETE
+
+router.post("/createPet", authenticate, createPet); // COMPLETE
+
+// PATCH methods for /user
+router.patch("/updateProfile", authenticate, updateUserProfile); // COMPLETE
+router.patch("/updateOwner/:id", authenticate, updateOwnerProfile); // COMPLETE
+
+// DELETE methods for /user
+router.delete("/deleteUser", authenticate, deleteUserProfile); // COMPLETE
+router.delete("/deleteOwner/:id", authenticate, deleteOwnerProfile); // COMPLETE
+
+// ----------------------------------------------------------------
+
+// Export router
 module.exports = router;
