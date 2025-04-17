5d98a04155719c2ffe1e820a765c69c404fb2eb7
HassanElMasry <h.elmasry256@gmail.com>
2025-04-17 23:02:23 +0200

    again0

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use(cookieParser());
 app.use(convertToLowerCase);
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? ["https://vetrik.vercel.app", /^https:\/\/vetrik.*\.vercel\.app$/]
-        : "http://localhost:3000",
+    origin: ["https://vetrik.vercel.app", /^https:\/\/vetrik.*\.vercel\.app$/],
+    //   process.env.NODE_ENV === "production"
+    //     ? ["https://vetrik.vercel.app", /^https:\/\/vetrik.*\.vercel\.app$/]
+    //     : "http://localhost:3000",
     credentials: true,
   })
 );
