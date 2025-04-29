d7afc0caf088db3a18fb513fcd453771803a453e
OmarElZaher <omarelzaher93@gmail.com>
2025-04-29 22:51:12 +0300

    Remove debug log for backend environment

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(convertToLowerCase);
-console.log(`Backend Environment: ${process.env.NODE_ENV}`);
 app.use(
 	cors({
 		origin: ["https://vetrik.vercel.app", /^https:\/\/vetrik.*\.vercel\.app$/],
